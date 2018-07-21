7fb8802084c7939b7240db6191f7851701fca613
idnavid <navidshokouhi68@gmail.com>
2018-07-21 15:41:10 +1000

    cleaned up data simulation function. NS

--- code/gen_data.py
+++ code/gen_data.py
@@ -8,26 +8,32 @@ import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
 
-def simulate_data(n_sources,n_micarrays,PLOT_ROOM=False):
+def simulate_data(n_sources,n_micarrays,fixed_position=False,PLOT_ROOM=False):
 	"""
 	Using pyroomacoustics to simulate
 	sound propagation in a shoebox room and record the result
 	to a wav file.
+	All of the microphone arrays consists of only two mics (left and right). 
 	Room dimensions are 6 x 6 x 2.4
 
 	Inputs: 
-		n_sources: number of sources (i.e., speakers)
-		n_micarrays: number of microphone arrays (= number_of_microphones/2)
-		PLOT_ROOM: plot simulated room. 
-				   there's currently a bug in the plots. I keep getting mirror
-				   images of the sources outside the room. 
+		n_sources:      number of sources (i.e., speakers)
+		n_micarrays:    number of microphone arrays (= number_of_microphones/2)
+		fixed_position: use a set of predetermined positions for the sources/microphones
+						If false, the simulater uses random positions. (default False)
+		PLOT_ROOM:      plot simulated room. 
+				        there's currently a bug in the plots. I keep getting mirror
+				        images of the sources outside the room. (default False)
 
 	Outputs:
 		array_recordings: numpy array containing all of the recorded signals. 
 						  [n_micarrays x 2 x number_of_samples]
+		fs: 			  sampling rate (Hz)
+		mic_positions:    numpy array containing microphone positions in xyz coordinates.
+						  [n_micarrays x 2 x 3]
+		source_positions: numpy array containing source positions in xyz coordinates.
+					      [n_sources x 3]
 	"""
-	
-
 
 	assert(n_sources>0 and n_sources<10)
 	assert(n_micarrays>0)
@@ -44,8 +50,14 @@ def simulate_data(n_sources,n_micarrays,PLOT_ROOM=False):
 
 	# Add Sources
 	for i in range(n_sources):
-		# position = np.array([np.random.randint(1,6),np.random.randint(1,6),np.random.uniform(1.8,2.1)]).T
-		position = np.array([5,5,2]).T
+		if not(fixed_position):
+			position = np.array([np.random.randint(1,6),np.random.randint(1,6),np.random.uniform(1.8,2.1)]).T
+		else:
+			# NOTE:
+			#   This scenario only works for a single position. 
+			#   I need to write a function for up to 10 positions. 
+			position = np.array([5,5,2]).T
+
 		source_positions.append(position)
 		room.add_source(position, delay=(10.0)*i,signal=source_signals[i])
 	
@@ -54,15 +66,25 @@ def simulate_data(n_sources,n_micarrays,PLOT_ROOM=False):
 	array_recordings = []
 	mic_positions = []
 	for m in range(n_micarrays):
-		# x1 = np.random.uniform(0.2,5.8)
-		# y1 = np.random.uniform(0.2,5.8)
-		x1 = 0.1
-		y1 = 0.1
+		# NOTE: 
+		#   I wasn't able to get pyroomacoustics to simulate
+		#   all microphone arrays together. So, I had to 
+		#   import them one by one and generate their responses.
+		if not(fixed_position):
+			x1 = np.random.uniform(0.2,5.8)
+			y1 = np.random.uniform(0.2,5.8)
+		else:
+			# NOTE:
+			#   This scenario only works for a single position. 
+			#   I need to write a function for up to 10 positions. 
+			x1 = 0.1
+			y1 = 0.1
+
 		z1 = 1.0 # array is always a 1.0 m high
 		x2 = x1
 		y2 = y1 + 0.1 # length of array is 0.1
 		z2 = z1 
-		R = np.array([[x1, x2], [y1, y2], [z1,  z2]])  # [[x], [y], [z]]
+		R = np.array([[x1, x2], [y1, y2], [z1,  z2]]) 
 		mic_positions.append(R)
 		room.add_microphone_array(pra.MicrophoneArray(R, room.fs))
 		
@@ -80,4 +102,5 @@ def simulate_data(n_sources,n_micarrays,PLOT_ROOM=False):
 	return np.array(array_recordings),fs,np.array(mic_positions),np.array(source_positions)
 
 if __name__=='__main__':
-	temp,fs,_,_ = simulate_data(1,4,PLOT_ROOM=False)
+	temp,fs,mic_array,source_array = simulate_data(8,12,fixed_position=False,PLOT_ROOM=False)
+	print(mic_array.shape,source_array.shape)
